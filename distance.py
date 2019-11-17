76f5dfe28c00eeb5680bd899b041ded3d3d3281d
netizen89 <nimmiv1991@gmail.com>
2019-11-17 13:55:40 +1100

    ReversingCommandlist/AnalysingCommand

--- distance.py
+++ distance.py
@@ -1,6 +1,61 @@
 from sys import argv
 
 
+# function to return minimum distance
+def find_distance(reversed_str) :
+    distance = 0
+    print(reversed_str)
+
+    for i in range(len(reversed_str)-1) :
+        print("value of index", i)
+        print("first element", reversed_str[i])
+        print("second element", reversed_str[i + 1])
+
+        #analysing first command is movement
+        if (reversed_str[i][0 :1] == "B" or reversed_str[i][0 :1]== "F") :
+            print("movement first")
+            print("second element after movement element", reversed_str[i + 1][0 :1])
+            if(reversed_str[i + 1][0 :1] == "F" or reversed_str[i + 1][0 :1] == "B"):
+                print("B,F together")
+                print("distance", distance)
+                #effective movemment:subtraction(direction-direction pair
+                if reversed_str[i][1 :2] >= reversed_str[i + 1][1 :2] :
+                    print("first element higher")
+                    distance = distance + (int(reversed_str[i][1 :2]) - int(reversed_str[i + 1][1 :2]))
+                    print("distance in first if", distance)
+
+
+                else:
+                    distance = distance + (int(reversed_str[i + 1][1 :2]) - int(reversed_str[i][1 :2]))
+            #incrementing index(element already considered
+
+                # effective movemment:addition(direction-movement pair
+            elif(reversed_str[i + 1][0 :1] == "L" or reversed_str[i + 1][0 :1] == "R"):
+                print("B/F and L/R together")
+                distance = distance + int(reversed_str[i][1 :2])
+
+            print("distance", distance)
+            #already computed pairs
+            i += 3
+         #analysis of first direction command
+
+        elif (reversed_str[i][0 :1] == "L" or reversed_str[i][0 :1]== "R") :
+            print("direction first")
+            print("inside elseif")
+                #checking for next command to be movement command:add distances
+            print("second element after direction element",reversed_str[i + 1][0:1])
+            if(reversed_str[i + 1][0 :1] == "B" or reversed_str[i+1][0 :1]== "F"):
+                print("loop check: after direction;movemnt command")
+                distance = distance + int(reversed_str[i+1][1 :2])
+             #no movement;just changing directions
+            else:
+                i = i+3
+                continue
+        print("distance", distance)
+
+    print("Minimum distance", distance)
+
+
 # function to analyse answer
 def analyse_command(snippet) :
     # direction and step variables
@@ -20,12 +75,16 @@ def analyse_command(snippet) :
             snippet[i] = "B" + snippet[i][1 :2]
         else :
             snippet[i] = "F" + snippet[i][1 :2]
-    print("modified as per solution", snippet)
+    print("reserved elements as per solution", snippet)
+    # reversing list to get back to starting position
+    snippet.reverse()
+
+    # finding distance
+    find_distance(snippet)
 
 
 
-
-# if __name__ == " __main__":
+# main
 commandString = argv[1]
 print(commandString)
 # gives a list of commands; to separate commas
